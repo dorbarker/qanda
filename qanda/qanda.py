deba3c3ec6487d0de8f7c0b5d75026f6c147c946
Dillon Barker <dor.barker@gmail.com>
2017-11-21 14:19:31 -0700

    Minimum working example

--- qanda/qanda.py
+++ qanda/qanda.py
@@ -1,29 +1,96 @@
 #!/usr/bin/env python3
 
+import argparse
 import functools
 import io
 import json
+import shlex
 import subprocess
 from pathlib import Path
-from typing import Dict, List, TypeVar
+from typing import Dict, List, Tuple, TypeVar
 
 import pandas as pd
-
-
-def arguments():
-    pass
-
-
-def main():
-    pass
-
 
 # type hints
 assembler_dict = Dict[str, str]
 PandasDataFrame = TypeVar('pandas.core.frame.DataFrame')
 
+# constants
+assembler_config_dir = Path.home() / '.local' / 'share' / 'qanda'
 
-def get_runinfo(query: str, database: str) -> PandasDataFrame:
+
+def arguments():
+
+    def get_assembler_backends():
+
+        return [p.stem for p in assembler_config_dir.glob('*.json')]
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--assembler',
+                        default='shovill',
+                        choices=get_assembler_backends(),
+                        help='Assembler backend [shovill]')
+
+    parser.add_argument('--database',
+                        default='sra',
+                        help='NCBI subject database [sra]')
+
+    parser.add_argument('--results',
+                        default=Path().resolve(),
+                        type=Path,
+                        help='Results directory ["."]')
+
+    parser.add_argument('--cores',
+                        default=1,
+                        type=int,
+                        help='Number of CPU cores [1]')
+
+    parser.add_argument('queries',
+                        nargs='+',
+                        help='NCBI queries')
+
+    return parser.parse_args()
+
+
+def main():
+
+    args = arguments()
+
+    qanda(args.queries, args.database, args.assembler, args.results, args.cores)
+
+
+def qanda(queries: List[str], database: str, assembler: str, results: Path, cores: int):
+
+    def setup_directories() -> Tuple[Path, Path, Path]:
+
+        fastq = results / 'fastqs'
+        assembly = results / 'assemblies'
+        biosample = results / 'biosamples'
+
+        fastq.mkdir(exist_ok=True)
+        assembly.mkdir(exist_ok=True)
+        biosample.mkdir(exist_ok=True)
+
+        return fastq, assembly, biosample
+
+    fastqs, assemblies, biosamples = setup_directories()
+
+    assembler_config = load_assembler(assembler_config_dir, assembler)
+
+    runinfo = build_runinfo_table(queries, database)
+
+    runinfo.to_csv(results / 'runinfo.csv')
+
+    # for acc in runinfo['Run']:
+    #     download_genome(acc, fastqs)
+
+    for acc in runinfo['Run']:
+        assemble(acc, fastqs, assemblies, cores, assembler_config)
+
+
+def get_metadata(query: str, database: str) -> PandasDataFrame:
+    # TODO add biosample fetching
 
     search = ('esearch', '-db', database, '-query', query)
 
@@ -33,21 +100,21 @@ def get_runinfo(query: str, database: str) -> PandasDataFrame:
 
     if database.lower() != 'sra':
 
-        link = ('elink', '-target', 'sra')
+        sra_link = ('elink', '-target', 'sra')
 
-        result = subprocess.run(link,
-                                input=search_result.stdout,
-                                check=True,
-                                stdout=subprocess.PIPE)
+        sra = subprocess.run(sra_link,
+                             input=search_result.stdout,
+                             check=True,
+                             stdout=subprocess.PIPE)
 
     else:
 
-        result = search_result
+        sra = search_result
 
     fetch = ('efetch', '-format', 'runinfo')
 
     runinfo = subprocess.run(fetch,
-                             input=result.stdout,
+                             input=sra.stdout,
                              check=True,
                              stdout=subprocess.PIPE)
 
@@ -56,7 +123,7 @@ def get_runinfo(query: str, database: str) -> PandasDataFrame:
 
 def build_runinfo_table(queries: List[str], database: str) -> PandasDataFrame:
 
-    runinfo_lines = (get_runinfo(query, database) for query in queries)
+    runinfo_lines = (get_metadata(query, database) for query in queries)
 
     runinfo_table = functools.reduce(pd.DataFrame.append, runinfo_lines)
 
@@ -68,25 +135,40 @@ def download_genome(acc: str, outdir: Path) -> None:
     out = str(outdir)
     fq_dump = ('fastq-dump', '--outdir', out, '--gzip', '--skip-technical',
                '--readids', '--read-filter', 'pass', '--dumpbase',
-               '--split-files', '--clip', acc)
+               '--origfmt', '--split-files', '--clip', acc)
 
     subprocess.run(fq_dump, check=True, stdout=subprocess.DEVNULL)
 
 
-def load_assembler(directory: Path, assembler: Path) -> assembler_dict:
+def load_assembler(directory: Path, assembler: str) -> assembler_dict:
 
     assembler_path = directory / assembler
 
-    with assembler_path.open('r') as f:
+    with assembler_path.with_suffix('.json').open('r') as f:
         assembler_config = json.load(f)
 
     return assembler_config
 
 
-def format_config(assembler_config: assembler_dict, options: Dict) -> assembler_dict:
+def assemble(accession: str, fastqs: Path, assemblies: Path, cores: int,
+             assembler_config: assembler_dict):
 
-    return {key: value.format(**options)
-            for key, value in assembler_config.items()}
+    fastq_template = '{}_pass_{{}}.fastq.gz'.format(accession)
+
+    options = {
+        'fwd':      fastqs / fastq_template.format(1),
+        'rev':      fastqs / fastq_template.format(2),
+        'outdir':   assemblies / accession,
+        'cores':    cores,
+    }
+
+    formatted_options = {key: value.format(**options)
+                         for key, value in assembler_config.items()
+                         if value}
+
+    for cmd in formatted_options:
+
+        subprocess.run(shlex.split(formatted_options[cmd]), check=True)
 
 
 if __name__ == '__main__':
